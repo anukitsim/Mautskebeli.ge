0f06b09e9b419b0aa27912f71174898469b87e1a
anukitsim <104069419+anukitsim@users.noreply.github.com>
2024-06-07 20:33:42 +0400

    fixed next config

--- next.config.mjs
+++ next.config.mjs
@@ -4,12 +4,6 @@ const nextConfig = {
     domains: ['mautskebeli.wpenginepowered.com', 'img.youtube.com', 'localhost'],
     formats: ['image/avif', 'image/webp'],
   },
-  lineHeight: {
-    'extra-loose': '2.5',  // Custom line height
-  },
-  fontFamily: {
-    'alk-tall-mtavruli': ['"ALK Tall Mtavruli"', 'sans-serif'],
-  },
 };
 
 export default nextConfig;
