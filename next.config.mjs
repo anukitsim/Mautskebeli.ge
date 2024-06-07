122c25caa7ebda4fc09b0eed21c7386cfa5b42fe
anukitsim <104069419+anukitsim@users.noreply.github.com>
2024-06-07 15:23:07 +0400

    second try

--- next.config.mjs
+++ next.config.mjs
@@ -1,20 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        domains: ['mautskebeli.wpenginepowered.com', 'img.youtube.com', 'localhost'],
-        
-          formats: ['image/avif', 'image/webp']
-        
-      },
-      lineHeight: {
-        'extra-loose': '2.5',  // Custom line height
-      },
-      fontFamily: {
-        'alk-tall-mtavruli': ['"ALK Tall Mtavruli"', 'sans-serif'],
-      },
-      experimental: {
-        appDir: true,
-      },
+  images: {
+    domains: ['mautskebeli.wpenginepowered.com', 'img.youtube.com', 'localhost'],
+    formats: ['image/avif', 'image/webp'],
+  },
+  experimental: {
+    appDir: true,
+  },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
