5c64e6087772e8ee3356b3342791b4c4aa9b06b3
anukitsim <104069419+anukitsim@users.noreply.github.com>
2024-06-13 01:39:58 +0400

    mau wignebi placement

--- app/text/page.jsx
+++ app/text/page.jsx
@@ -6,14 +6,14 @@ const page = () => {
   return (
     <>
       <section className='w-10/12 mx-auto flex lg:flex-row flex-col gap-[20px] mt-[100px]'>
-        <Link href='/books' className='lg:w-4/12 w-full hover:scale-105 h-[280px] rounded-[16px] border border-[#E0DBE8] flex flex-col justify-center items-center'>
+      <Link  href='all-articles' className='lg:w-4/12 hover:scale-105 w-full h-[280px] flex-col rounded-[16px] border border-[#E0DBE8] flex justify-center items-center'>
         <Image
-              src="/images/mau-wignebi.svg"
+              src="/images/mau-statiebi.svg"
               alt="donation"
               width={270}
               height={180}
             />
-            <p>მაუწყებელი წიგნები</p>
+             <p>სტატიები</p>
         </Link>
         <Link href='/translate' className='lg:w-4/12 w-full hover:scale-105 h-[280px] flex-col rounded-[16px] border border-[#E0DBE8] flex justify-center items-center'>
         <Image
@@ -24,15 +24,18 @@ const page = () => {
             />
              <p>თარგმანი</p>
         </Link>
-        <Link  href='all-articles' className='lg:w-4/12 hover:scale-105 w-full h-[280px] flex-col rounded-[16px] border border-[#E0DBE8] flex justify-center items-center'>
+        <Link href='/books' className='lg:w-4/12 w-full hover:scale-105 h-[280px] rounded-[16px] border border-[#E0DBE8] flex flex-col justify-center items-center'>
         <Image
-              src="/images/mau-statiebi.svg"
+              src="/images/mau-wignebi.svg"
               alt="donation"
               width={270}
               height={180}
             />
-             <p>სტატიები</p>
+            <p>მაუწყებელი წიგნები</p>
         </Link>
+       
+        
+        
     
     </section>
     <Footer />
