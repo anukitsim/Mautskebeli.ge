ecaf620d068f5d34e327fe1414c0296f20e2a5ce
anukitsim <104069419+anukitsim@users.noreply.github.com>
2024-06-23 20:39:02 +0400

    fixed translate mobile layout

--- app/(root)/layout.js
+++ app/(root)/layout.js
@@ -11,6 +11,8 @@ export const metadata = {
 };
 
 export default function RootLayout({ children }) {
+  const defaultOgImage = 'https://mautskebeli.ge/api/og?title=Default%20Title';
+
   return (
     <html lang="en">
       <Head>
@@ -18,16 +20,17 @@ export default function RootLayout({ children }) {
         <meta name="description" content={metadata.description} />
         <meta property="og:title" content={metadata.title} />
         <meta property="og:description" content={metadata.description} />
-        <meta property="og:image" content="https://www.mautskebeli.ge/images/og-logo.jpg" />
+        <meta property="og:image" content={defaultOgImage} />
         <meta property="og:image:width" content="1200" />
         <meta property="og:image:height" content="630" />
-        <meta property="og:image:type" content="image/jpeg" />
-        <meta property="og:image:alt" content="Mautskebeli Logo" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1, user-scalable=no" />
-        <link rel="icon" href="/favicon.ico" />
       </Head>
+      <head>
+      <link rel="icon" href="/favicon.ico" />
+      </head>
       <body>
+       
         {children}
+      
       </body>
     </html>
   );
--- app/translate/page.jsx
+++ app/translate/page.jsx
@@ -44,17 +44,17 @@ const AllArticlesList = () => {
   }, []);
 
   return (
-    <section className="mx-auto] flex flex-col">
-      <div className="w-full sm:w-10/12 flex items-center justify-between lg:mt-20 mt-[42px] mx-auto pl-4 pr-4 lg:pl-2 lg:pr-2">
-        <p className="text-[#474F7A] text-[24px] font-bold">თარგმანი</p>
+    <section className="container mx-auto flex flex-col p-4">
+      <div className="w-full flex items-center justify-between mt-8 sm:mt-12 lg:mt-20">
+        <p className="text-[#474F7A] text-2xl sm:text-3xl font-bold">თარგმანი</p>
       </div>
-      <div className="w-10/12 mx-auto grid gap-5 mt-5 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3">
+      <div className="w-full grid gap-5 mt-5 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3">
         {articles.map(article => (
           <Link href={`/translate/${article.id}`} passHref key={article.id}>
-            <div className="article bg-[#F6F4F8] rounded-tl-[10px] rounded-tr-[10px] border border-[#B6A8CD] overflow-hidden">
-              <div className="article-image-container relative w-full h-[200px]">
+            <div className="article bg-[#F6F4F8] rounded-lg border border-[#B6A8CD] overflow-hidden">
+              <div className="relative w-full h-48 sm:h-56 lg:h-64">
                 <Image
-                  src={article.acf.image || '/images/default-image.png'}
+                  src={article.acf.image ? article.acf.image : '/images/default-image.png'}
                   alt="article-cover"
                   fill
                   sizes="(max-width: 768px) 100vw, 33vw"
@@ -62,19 +62,18 @@ const AllArticlesList = () => {
                   className="article-image"
                 />
               </div>
-              <div className="p-[18px]">
-                <h2 className="text-[20px] font-bold mb-2" style={{ color: '#474F7A' }}>
+              <div className="p-4 sm:p-6">
+                <h2 className="text-lg sm:text-xl font-bold mb-2 text-[#474F7A]">
                   {article.title.rendered}
                 </h2>
-                <span className="text-[#8D91AB] text-[14px] font-bold">
+                <span className="text-[#8D91AB] text-sm sm:text-base font-bold">
                   {truncateText(article.acf.title, 10)}
                 </span>
-              
-                <div className="flex flex-col justify-end pt-[30px] items-end">
-                  <span className="text-[15px] text-[#AD88C6]">
+                <div className="flex flex-col justify-end items-end mt-4 sm:mt-6">
+                  <span className="text-sm sm:text-base text-[#AD88C6]">
                     {article.acf.translator} 
                   </span>
-                  <button className="text-white text-[12px] mt-[16px] bg-[#AD88C6] rounded-[6px] pt-[10px] pb-[10px] pl-[12px] pr-[12px]">
+                  <button className="text-white text-xs sm:text-sm mt-4 bg-[#AD88C6] rounded-md py-2 px-3">
                     ნახეთ სრულად
                   </button>
                 </div>
