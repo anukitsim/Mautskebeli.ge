69651e6d30adfa7a815aa3d8d46b336f5d2c214c
anukitsim <104069419+anukitsim@users.noreply.github.com>
2024-07-09 20:48:12 +0400

    updated books section share functionality

--- app/books/[id]/page.jsx
+++ app/books/[id]/page.jsx
@@ -1,17 +1,81 @@
 import dynamic from 'next/dynamic';
 
-const NoSSRBookPage = dynamic(() => import('./BookPage'), { ssr: false });
+export async function generateMetadata({ params }) {
+  const { id } = params;
 
-async function getData(id) {
-  const res = await fetch(`https://mautskebeli.wpenginepowered.com/wp-json/wp/v2/mau-books/${id}?acf_format=standard&_fields=id,title,acf,date`);
-  if (!res.ok) {
-    throw new Error('Failed to fetch data');
+  try {
+    const apiUrl = `https://www.mautskebeli.ge/api/book-og?id=${id}`;
+    console.log(`Fetching OG tags from: ${apiUrl}`);
+
+    const res = await fetch(apiUrl);
+
+    if (!res.ok) {
+      console.error(`Error fetching OG tags: ${res.statusText}`);
+      return {
+        title: 'Default Title',
+        description: 'Default description',
+        openGraph: {
+          title: 'Default Title',
+          description: 'Default description',
+          url: `https://www.mautskebeli.ge/books/${id}`,
+          images: [
+            {
+              url: '/images/default-og-image.jpg',
+              width: 1200,
+              height: 630,
+            },
+          ],
+          type: 'article',
+        },
+      };
+    }
+
+    const ogTags = await res.json();
+    console.log('OG Tags:', ogTags);
+
+    return {
+      title: ogTags.title,
+      description: ogTags.description,
+      openGraph: {
+        title: ogTags.title,
+        description: ogTags.description,
+        url: ogTags.url,
+        images: [
+          {
+            url: ogTags.image,
+            width: 1200,
+            height: 630,
+          },
+        ],
+        type: 'article',
+      },
+    };
+  } catch (error) {
+    console.error('Unexpected error fetching metadata:', error);
+    return {
+      title: 'Default Title',
+      description: 'Default description',
+      openGraph: {
+        title: 'Default Title',
+        description: 'Default description',
+        url: `https://www.mautskebeli.ge/books/${id}`,
+        images: [
+          {
+            url: '/images/default-og-image.jpg',
+            width: 1200,
+            height: 630,
+          },
+        ],
+        type: 'article',
+      },
+    };
   }
-  return res.json();
 }
 
-export default async function Page({ params }) {
-  const data = await getData(params.id);
+const NoSSRArticlePage = dynamic(() => import('./BookPage'), { ssr: false });
 
-  return <NoSSRBookPage params={params} data={data} />;
-}
+const Page = ({ params }) => {
+  return <NoSSRArticlePage params={params} />;
+};
+
+export default Page;
--- app/books/layout.jsx
+++ app/books/layout.jsx
@@ -1,15 +1,29 @@
-'use client';
-
 import "../../style/globals.css";
-import React from "react";
 import Header from "../components/Header";
 import Navigation from "../components/Navigation";
-import { MenuProvider } from "../context/MenuContext";
+import { MenuProvider } from "@/app/context/MenuContext";
 import Footer from "../components/Footer";
+import Head from 'next/head';
+
+export const metadata = {
+  title: "mautskebeli.ge",
+  description: "მედია პლათფორმა მაუწყებელი",
+  ogImage: "https://mautskebeli.ge/api/og?title=Default%20Title",
+  metadataBase: new URL('https://www.mautskebeli.ge'), // Set your base URL here
+};
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <Head>
+        <title>{metadata.title}</title>
+        <meta name="description" content={metadata.description} />
+        <meta property="og:title" content={metadata.title} />
+        <meta property="og:description" content={metadata.description} />
+        <meta property="og:image" content={metadata.ogImage} />
+        <meta property="og:image:width" content="1200" />
+        <meta property="og:image:height" content="630" />
+      </Head>
       <head>
       <link rel="icon" href="/favicon.ico" />
       </head>
@@ -19,9 +33,11 @@ export default function RootLayout({ children }) {
             <Header />
             <Navigation />
           </div>
-          {children}
-          <Footer />
         </MenuProvider>
+        {children}
+        <div className="mt-40">
+          <Footer />
+        </div>
       </body>
     </html>
   );
