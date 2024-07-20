4805c292829851cb9a0636bca292798f8abdc8a7
anukitsim <104069419+anukitsim@users.noreply.github.com>
2024-07-20 20:22:09 +0400

    footer icon sizes fix

--- app/components/Footer.jsx
+++ app/components/Footer.jsx
@@ -47,20 +47,20 @@ const Footer = () => {
 
   return (
     <>
-      <footer className="lg:flex hidden w-9/12 h-[300px] mb-[35px] mx-auto mt-[69px]  gap-[40px]">
+      <footer className="lg:flex hidden w-9/12 h-[300px] mb-[35px] mx-auto mt-[69px] gap-[40px]">
         {/* Desktop version of the footer */}
         <div className="h-full w-1/2 flex flex-col gap-[18px]">
           <h1 className="text-[#AD88C6] text-[16px] font-bold">კონტაქტი</h1>
           <div className="flex gap-[10px] text-[#474F7A] text-[16px]">
-            <Image src="/images/mail.svg" alt="mail" width={0} height={0} style={{ width: "auto", height: "20px" }} />
+            <Image src="/images/mail.svg" alt="mail" width={20} height={20} objectFit="contain" />
             <a href="mailto:platformforsj@gmail.com" className="text-[16px]">platformforsj@gmail.com</a>
           </div>
           <div className="flex gap-[10px] text-[#474F7A] text-[16px]">
-            <Image src="/images/phone.svg" alt="phone" width={0} height={0} style={{ width: "auto", height: "20px" }} />
+            <Image src="/images/phone.svg" alt="phone" width={20} height={20} objectFit="contain" />
             <a href="tel:+995574831085" className="text-[16px]">+995 574 83 10 85</a>
           </div>
           <div className="flex gap-[10px] text-[#474F7A]">
-            <Image src="/images/location.svg" alt="location" width={0} height={0} style={{ width: 'auto', height: '20px' }} />
+            <Image src="/images/location.svg" alt="location" width={20} height={20} objectFit="contain" />
             <a href="https://www.google.com/maps/search/?api=1&query=Kostava+30,+Batumi" target="_blank" className="text-[16px] whitespace-nowrap">ბათუმი, კოსტავას 30</a>
           </div>
           <Image src="/images/logo.svg" alt="logo" width={116} height={32} className="mt-[32px]" />
@@ -68,42 +68,37 @@ const Footer = () => {
         <div className="h-full w-1/3 flex flex-col gap-[18px]">
           <h1 className="text-[#AD88C6] text-[16px] font-bold">ნავიგაცია</h1>
           <Link href="/about-us" className="text-[#474F7A]">ჩვენ შესახებ</Link>
-          
           <Link href="/all-videos" className="text-[#474F7A]">ვიდეოები</Link>
           <Link href="/text" className="text-[#474F7A]">ტექსტი</Link>
           <Link href="/sporti" className="text-[#474F7A]">სპორტი</Link>
-          <Link href='/bolo-ambebi' className="text-[#474F7A]">ამბები</Link>
         </div>
         <div className="h-full w-1/3 flex flex-col gap-[16px]">
-        <h1 className="text-[#AD88C6] text-[16px] font-bold">გამოგვიწერე</h1>
-      
-        <Link href='https://www.facebook.com/mautskebeli.ge' target='_blank' className="flex gap-[10px] text-[16px] text-[#474F7A]">
-            <Image src="/images/fb.svg" alt="mail" width={20} height={20} />
+          <h1 className="text-[#AD88C6] text-[16px] font-bold">გამოგვიწერე</h1>
+          <Link href='https://www.facebook.com/mautskebeli.ge' target='_blank' className="flex gap-[10px] text-[16px] text-[#474F7A]">
+            <Image src="/images/fb.svg" alt="Facebook" width={25} height={10} objectFit="contain" />
             <span>Facebook</span>
           </Link>
           <Link href='https://www.youtube.com/@mautskebeli' target='_blank' className="flex gap-[10px] text-[16px] text-[#474F7A]">
-            <Image src="/images/yt.svg" alt="mail" width={20} height={20} />
+            <Image src="/images/yt.svg" alt="YouTube" width={25} height={0} objectFit="contain" />
             <span>YouTube</span>
           </Link>
-          <Link href='https://www.instagram.com/mautskebeli.ge/' target='_blank' className="flex gap-[16px] text-[#474F7A]">
-            <Image src="/images/instagram.svg" alt="mail" width={20} height={20} />
-            <span className="text-[16px] whitespace-nowrap">Instagram</span>
+          <Link href='https://www.instagram.com/mautskebeli.ge/' target='_blank' className="flex gap-[10px] text-[16px] text-[#474F7A]">
+            <Image src="/images/instagram.svg" alt="Instagram" width={25} height={20} objectFit="contain" />
+            <span>Instagram</span>
           </Link>
-          <Link href='https://www.tiktok.com/@mautskebeli.ge' target='_blank' className="flex gap-[16px] text-[16px] text-[#474F7A]">
-            <Image src="/images/tiktok.svg" alt="mail" width={20} height={20} />
+          <Link href='https://www.tiktok.com/@mautskebeli.ge' target='_blank' className="flex gap-[10px] text-[16px] text-[#474F7A]">
+            <Image src="/images/tiktok.svg" alt="TikTok" width={25} height={20} objectFit="contain" />
             <span>TikTok</span>
           </Link>
-          <Link href='https://x.com/mautskebeli' target='_blank' className="flex gap-[16px] text-[#474F7A] text-[16px]">
-            <Image src="/images/x.svg" alt="mail" width={20} height={20} />
+          <Link href='https://x.com/mautskebeli' target='_blank' className="flex gap-[10px] text-[16px] text-[#474F7A]">
+            <Image src="/images/x.svg" alt="X Twitter" width={25} height={20} objectFit="contain" />
             <span>X Twitter</span>
           </Link>
           <div className="flex gap-[10px] text-[16px] text-[#474F7A]">
-            <Image src="/images/telegram.svg" alt="mail" width={20} height={20} />
+            <Image src="/images/telegram.svg" alt="Telegram" width={25} height={20} objectFit="contain" />
             <span>Telegram</span>
           </div>
-     
-      </div>
-
+        </div>
         <div className="h-full w-1/2 flex flex-col gap-[16px]">
           <h1 className="text-[#AD88C6] text-[16px] font-bold">გამოიწერე სიახლეები</h1>
           {/* Subscription form for Desktop */}
@@ -146,18 +141,15 @@ const Footer = () => {
           >
             საწყისზე დაბრუნება
             <div className="flex justify-center items-center ml-2">
-              {/* Adjust width and height as per your image's aspect ratio */}
               <Image
                 src="/images/top.svg"
                 alt="Scroll to top"
-                width={0}
-                height={0}
-                style={{ width: "auto", height: "20px" }}
+                width={20}
+                height={20}
               />
             </div>
           </button>
         </div>
-        
       </footer>
 
       {/* Mobile screen Footer */}
@@ -166,87 +158,87 @@ const Footer = () => {
         <div className="flex flex-col justify-center items-center gap-[18px]">
           <h1 className="text-white text-[15px] font-bold">კონტაქტი</h1>
           <div className="flex gap-[10px]  text-[#474F7A] text-[15px]">
-            <Image src="/images/mail.svg" alt="mail" width={0} height={0} style={{ width: "auto", height: "20px" }} />
+            <Image src="/images/mail.svg" alt="mail" width={20} height={20} objectFit="contain" />
             <a href="mailto:platformforsj@gmail.com" className="text-[16px]">platformforsj@gmail.com</a>
           </div>
           <div className="flex gap-[10px]  text-[#474F7A] text-[15px]">
-            <Image src="/images/phone.svg" alt="phone" width={0} height={0} style={{ width: "auto", height: "20px" }} />
+            <Image src="/images/phone.svg" alt="phone" width={20} height={20} objectFit="contain" />
             <a href="tel:+995574831085" className="text-[16px]">+995 574 83 10 85</a>
           </div>
           <div className="flex gap-[10px]  text-[#474F7A] text-[15px]">
-            <Image src="/images/location.svg" alt="location" width={0} height={0} style={{ width: 'auto', height: '20px' }} />
+            <Image src="/images/location.svg" alt="location" width={20} height={20} objectFit="contain" />
             <a href="https://www.google.com/maps/search/?api=1&query=Kostava+30,+Batumi" target="_blank" className="text-[16px] whitespace-nowrap">ბათუმი, კოსტავას 30</a>
           </div>
         </div>
         <div className="flex flex-col justify-center items-center gap-[18px]">
           <h1 className="text-white text-[15px] font-bold">გამოგვიწერე</h1>
           <div className="flex gap-[16px] text-[#474F7A]">
-            <Link  href='https://www.facebook.com/mautskebeli.ge' target='_blank' className="flex gap-[6px] text-[16px]">
+            <Link href='https://www.facebook.com/mautskebeli.ge' target='_blank' className="flex gap-[6px] text-[16px]">
               <Image
                 src="/images/facebook-mobile.svg"
-                alt="mail"
-                width={0}
-                height={0}
-                style={{ width: "auto", height: "20px" }}
+                alt="Facebook"
+                width={25}
+                height={20}
+                objectFit="contain"
               />
               <span>Facebook</span>
             </Link>
             <Link href='https://www.youtube.com/@mautskebeli' target='_blank' className="flex gap-[6px] text-[16px]">
               <Image
                 src="/images/youtube-mobile.svg"
-                alt="mail"
-                width={0}
-                height={0}
-                style={{ width: "auto", height: "20px" }}
+                alt="YouTube"
+                width={25}
+                height={20}
+                objectFit="contain"
               />
               <span>YouTube</span>
             </Link>
             <Link href='https://www.instagram.com/mautskebeli.ge/' target='_blank' className="flex gap-[6px] text-[16px]">
               <Image
                 src="/images/instagram-mobile.svg"
-                alt="mail"
-                width={0}
-                height={0}
-                style={{ width: "auto", height: "20px" }}
+                alt="Instagram"
+                width={25}
+                height={20}
+                objectFit="contain"
               />
-              <span className="text-[16px] whitespace-nowrap">Instagram</span>
+              <span>Instagram</span>
             </Link>
           </div>
           <div className="flex gap-[16px] text-[#474F7A]">
             <Link href='https://www.tiktok.com/@mautskebeli.ge' target='_blank' className="flex gap-[6px] text-[16px]">
               <Image
                 src="/images/tiktok-mobile.svg"
-                alt="mail"
-                width={0}
-                height={0}
-                style={{ width: "auto", height: "20px" }}
+                alt="TikTok"
+                width={25}
+                height={20}
+                objectFit="contain"
               />
               <span>TikTok</span>
             </Link>
             <Link href='https://x.com/mautskebeli' target='_blank' className="flex gap-[6px] text-[16px]">
               <Image
                 src="/images/twitter-mobile.svg"
-                alt="mail"
-                width={0}
-                height={0}
-                style={{ width: "auto", height: "20px" }}
+                alt="X Twitter"
+                width={25}
+                height={20}
+                objectFit="contain"
               />
               <span>X Twitter</span>
             </Link>
             <div className="flex gap-[6px] text-[16px]">
               <Image
                 src="/images/telegram-mobile.svg"
-                alt="mail"
-                width={0}
-                height={0}
-                style={{ width: "auto", height: "20px" }}
+                alt="Telegram"
+                width={25}
+                height={20}
+                objectFit="contain"
               />
               <span>Telegram</span>
             </div>
-            </div>
+          </div>
         </div>
         <div className="flex flex-col justify-center items-center gap-[18px]">
-          <h1 className="text-white text-[15px] font-bold">გამოგვიწერე</h1>
+          <h1 className="text-white text-[15px] font-bold">გამოიწერე</h1>
           {/* Subscription form for Mobile */}
           <form onSubmit={handleSubmit} className="flex flex-col w-full gap-[10px]">
             <input
@@ -281,41 +273,35 @@ const Footer = () => {
               გამოიწერე
             </button>
             {message && <p className="text-[#6E6893] text-[14px] mt-4">{message}</p>}
-            
           </form>
         </div>
         <div className="flex flex-col justify-center items-center gap-[18px]">
           <h1 className="text-white text-[15px] font-bold">ნავიგაცია</h1>
           <div className="flex gap-[42px]">
             <div className="flex flex-col gap-[12px] text-[#474F7A] text-[15px]">
-            <Link href="/about-us">ჩვენ შესახებ</Link>
-          
-          <Link href="/all-videos">ვიდეოები</Link>
-          <Link href="/text">ტექსტი</Link>
+              <Link href="/about-us">ჩვენ შესახებ</Link>
+              <Link href="/all-videos">ვიდეოები</Link>
             </div>
             <div className="flex flex-col gap-[12px] text-[#474F7A] text-[15px]">
-           
-          <Link href="/sporti">სპორტი</Link>
-          <Link href='/bolo-ambebi'>ამბები</Link>
+              <Link href="/sporti">სპორტი</Link>
+              <Link href="/text">ტექსტი</Link>
             </div>
-            
           </div>
           <button
-              onClick={scrollToTop}
-              className="mt-[35px] self-end justify-self-end p-2 text-[#474F7A] text-[16px] cursor-pointer flex items-center justify-center"
-              aria-label="Scroll to top"
-            >
-              საწყისზე დაბრუნება
-              <div className="flex justify-center items-center ml-2">
-                {/* Adjust width and height as per your image's aspect ratio */}
-                <Image
-                  src="/images/top.svg"
-                  alt="Scroll to top"
-                  width={20}
-                  height={20}
-                />
-              </div>
-            </button>
+            onClick={scrollToTop}
+            className="mt-[35px] self-end justify-self-end p-2 text-[#474F7A] text-[16px] cursor-pointer flex items-center justify-center"
+            aria-label="Scroll to top"
+          >
+            საწყისზე დაბრუნება
+            <div className="flex justify-center items-center ml-2">
+              <Image
+                src="/images/top.svg"
+                alt="Scroll to top"
+                width={20}
+                height={20}
+              />
+            </div>
+          </button>
         </div>
       </footer>
     </>
