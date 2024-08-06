0b9dd09d41848c09fa7d8f97ae5452a372d51545
anukitsim <104069419+anukitsim@users.noreply.github.com>
2024-08-06 23:51:36 +0400

    returning dinamic og images

--- app/(root)/api/og/route.js
+++ app/(root)/api/og/route.js
@@ -5,10 +5,7 @@ export const runtime = 'edge';
 export default function handler(req) {
   const { searchParams } = new URL(req.url);
   const title = searchParams.get('title') || 'Default Title';
-  // const imageUrl = searchParams.get('image') || 'https://mautskebeli.ge/api/og?title=Default%20Title';
-
-  const imageUrl = "https://images.pexels.com/photos/1502062/pexels-photo-1502062.jpeg?cs=srgb&dl=pexels-jess-bailey-designs-1502062.jpg&fm=jpg";
-
+  const imageUrl = searchParams.get('image') || 'https://mautskebeli.ge/api/og?title=Default%20Title';
 
   return new ImageResponse(
     (
