356046c3e430e7fd75b4f4314fe894bfd2506ba5
anukitsim <104069419+anukitsim@users.noreply.github.com>
2024-08-10 00:04:09 +0400

    fix(og-tags): Ensure consistent image dimensions and URL for Facebook previews

--- app/all-articles/[id]/ArticlePage.jsx
+++ app/all-articles/[id]/ArticlePage.jsx
@@ -1,4 +1,5 @@
-"use client";
+"use client"
+
 import { useEffect, useState, useRef, useCallback } from 'react';
 import Image from 'next/image';
 import Head from 'next/head';
@@ -117,59 +118,58 @@ const ArticlePage = ({ params }) => {
   return (
     <>
     <Head>
-  <title>{article.title.rendered}</title>
-  <meta name="description" content={ogDescription} />
-  <meta property="og:url" content={articleUrl} />
-  <meta property="og:type" content="article" />
-  <meta property="og:title" content={article.title.rendered} />
-  <meta property="og:description" content={ogDescription} />
-  <meta property="og:image" content={`${ogImage}?cb=${new Date().getTime()}`} />
-  <meta property="og:image:width" content="1200" />
-  <meta property="og:image:height" content="630" />
-  <meta property="fb:app_id" content="2191957607826649" />
-  <meta property="og:image:alt" content={article.title.rendered} />
-</Head>
+      <title>{article.title.rendered}</title>
+      <meta name="description" content={ogDescription} />
+      <meta property="og:url" content={articleUrl} />
+      <meta property="og:type" content="article" />
+      <meta property="og:title" content={article.title.rendered} />
+      <meta property="og:description" content={ogDescription} />
+      <meta property="og:image" content={ogImage} />
+      <meta property="og:image:width" content="1200" />
+      <meta property="og:image:height" content="630" />
+      <meta property="fb:app_id" content="2191957607826649" />
+      <meta property="og:image:alt" content={article.title.rendered} />
+    </Head>
 
-
-      <section className="w-full mx-auto mt-10 px-4 lg:px-0 overflow-x-hidden relative">
-        <div className="w-full lg:w-[54%] mx-auto bg-opacity-90 p-5 rounded-lg">
-          <div className="w-full h-auto mb-5">
-            <Image src={article.acf.image || '/images/default-og-image.jpg'} alt={article.title.rendered} width={800} height={450} style={{ objectFit: 'cover' }} className="rounded-lg w-full" />
-            <h1 className="font-alk-tall-mtavruli text-[32px] sm:text-[64px] font-light leading-none text-[#474F7A] mt-[24px] mb-5">{article.title.rendered}</h1>
-            <h3 className="font-alk-tall-mtavruli text-[32px] sm:text-[34px] font-light leading-wide text-[#474F7A] mt-[24px] mb-5">{article.acf.sub_title}</h3>
-            <h2 className="font-noto-sans-georgian text-[16px] sm:text-[24px] font-extrabold text-[#AD88C6] leading-normal mb-5">{article.acf['ავტორი']}</h2>
-            <p className="text-[#474F7A] font-semibold pb-10">{article.formattedDate}</p>
-          </div>
-          <div ref={articleContentRef} className="article-content text-[#474F7A] text-wrap w-full font-noto-sans-georgian text-[14px] sm:text-[16px] font-normal lg:text-justify leading-[30px] sm:leading-[35px] tracking-[0.32px]"></div>
-          <div className="flex flex-wrap gap-4 mt-10">
-            <button onClick={() => setShowShareOptions(true)} className="bg-[#FECE27] text-[#474F7A] pl-[18px] pr-[18px] pt-[4px] pb-[4px] text-[16px] font-seibold rounded flex gap-[12px] items-center justify-center">
-              <Image src="/images/share.png" alt="share icon" width={24} height={24} />
-            </button>
-          </div>
-          {showShareOptions && (
-            <div className="fixed inset-0 bg-gray-800 bg-opacity-50 flex items-center justify-center">
-              <div ref={shareOptionsRef} className="rounded-lg p-6 w-80" style={{ backgroundColor: "rgba(0, 0, 0, 0.30)" }}>
-                <h2 className="text-xl text-white font-bold mb-4">გააზიარე</h2>
-                <div className="flex items-center pt-7 gap-5">
-                  <FacebookShareButton url={articleUrl} quote={article.title.rendered}>
-                    <FacebookIcon size={44} round={true} />
-                  </FacebookShareButton>
-                  <TwitterShareButton url={articleUrl} title={article.title.rendered}>
-                    <TwitterIcon size={44} round={true} />
-                  </TwitterShareButton>
-                </div>
+    <section className="w-full mx-auto mt-10 px-4 lg:px-0 overflow-x-hidden relative">
+      <div className="w-full lg:w-[54%] mx-auto bg-opacity-90 p-5 rounded-lg">
+        <div className="w-full h-auto mb-5">
+          <Image src={article.acf.image || '/images/default-og-image.jpg'} alt={article.title.rendered} width={800} height={450} style={{ objectFit: 'cover' }} className="rounded-lg w-full" />
+          <h1 className="font-alk-tall-mtavruli text-[32px] sm:text-[64px] font-light leading-none text-[#474F7A] mt-[24px] mb-5">{article.title.rendered}</h1>
+          <h3 className="font-alk-tall-mtavruli text-[32px] sm:text-[34px] font-light leading-wide text-[#474F7A] mt-[24px] mb-5">{article.acf.sub_title}</h3>
+          <h2 className="font-noto-sans-georgian text-[16px] sm:text-[24px] font-extrabold text-[#AD88C6] leading-normal mb-5">{article.acf['ავტორი']}</h2>
+          <p className="text-[#474F7A] font-semibold pb-10">{article.formattedDate}</p>
+        </div>
+        <div ref={articleContentRef} className="article-content text-[#474F7A] text-wrap w-full font-noto-sans-georgian text-[14px] sm:text-[16px] font-normal lg:text-justify leading-[30px] sm:leading-[35px] tracking-[0.32px]"></div>
+        <div className="flex flex-wrap gap-4 mt-10">
+          <button onClick={() => setShowShareOptions(true)} className="bg-[#FECE27] text-[#474F7A] pl-[18px] pr-[18px] pt-[4px] pb-[4px] text-[16px] font-seibold rounded flex gap-[12px] items-center justify-center">
+            <Image src="/images/share.png" alt="share icon" width={24} height={24} />
+          </button>
+        </div>
+        {showShareOptions && (
+          <div className="fixed inset-0 bg-gray-800 bg-opacity-50 flex items-center justify-center">
+            <div ref={shareOptionsRef} className="rounded-lg p-6 w-80" style={{ backgroundColor: "rgba(0, 0, 0, 0.30)" }}>
+              <h2 className="text-xl text-white font-bold mb-4">გააზიარე</h2>
+              <div className="flex items-center pt-7 gap-5">
+                <FacebookShareButton url={articleUrl} quote={article.title.rendered}>
+                  <FacebookIcon size={44} round={true} />
+                </FacebookShareButton>
+                <TwitterShareButton url={articleUrl} title={article.title.rendered}>
+                  <TwitterIcon size={44} round={true} />
+                </TwitterShareButton>
               </div>
             </div>
-          )}
-        </div>
-        {showScrollButton && (
-          <button onClick={scrollToTop} className="fixed bottom-10 right-10 text-[#474F7A] w-[210px] h-[51px] rounded-[100px] inline-flex items-center justify-center gap-[10px]"
-            style={{ padding: '25px 32px 28px 32px', background: '#FECE27', boxShadow: '0 0 20px 5px rgba(254, 206, 39, 0.5)' }}>
-            <span className="whitespace-nowrap">საწყისზე დაბრუნება</span>
-          </button>
+          </div>
         )}
-        <footer ref={footerRef} className="h-[100px]"></footer>
-      </section>
+      </div>
+      {showScrollButton && (
+        <button onClick={scrollToTop} className="fixed bottom-10 right-10 text-[#474F7A] w-[210px] h-[51px] rounded-[100px] inline-flex items-center justify-center gap-[10px]"
+          style={{ padding: '25px 32px 28px 32px', background: '#FECE27', boxShadow: '0 0 20px 5px rgba(254, 206, 39, 0.5)' }}>
+          <span className="whitespace-nowrap">საწყისზე დაბრუნება</span>
+        </button>
+      )}
+      <footer ref={footerRef} className="h-[100px]"></footer>
+    </section>
     </>
   );
 };
