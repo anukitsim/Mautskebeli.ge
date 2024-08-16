7d4b21774183cf4fa8841f82b719e6cd0543958c
anukitsim <104069419+anukitsim@users.noreply.github.com>
2024-08-16 14:16:24 +0400

    not sure

--- app/(root)/api/og/route.js
+++ app/(root)/api/og/route.js
@@ -1,37 +1,40 @@
-import { ImageResponse } from '@vercel/og';
-
-export const runtime = 'edge';
-
 export default function handler(req) {
-  const { searchParams } = new URL(req.url);
-  const title = searchParams.get('title') || 'Default Title';
-  const imageUrl = searchParams.get('image') || 'https://mautskebeli.ge/api/og?title=Default%20Title';
+  try {
+    const { searchParams } = new URL(req.url);
+    const title = searchParams.get('title') || 'Default Title';
+    const imageUrl = searchParams.get('image') || 'https://mautskebeli.ge/api/og?title=Default%20Title';
 
-  // Generate a cache-busting parameter based on timestamp
-  const cacheBuster = new Date().getTime();
+    console.log("Generating image with title:", title);
+    console.log("Using image URL:", imageUrl);
 
-  return new ImageResponse(
-    (
-      <div
-        style={{
-          fontSize: 60,
-          color: 'white',
-          background: `url(${encodeURIComponent(imageUrl)}?cb=${cacheBuster}) no-repeat center center`,
-          backgroundSize: 'cover',
-          width: '100%',
-          height: '100%',
-          padding: '40px',
-          display: 'flex',
-          alignItems: 'center',
-          justifyContent: 'center',
-        }}
-      >
-        {title}
-      </div>
-    ),
-    {
-      width: 1200,
-      height: 630,
-    }
-  );
+    const cacheBuster = new Date().getTime();
+
+    return new ImageResponse(
+      (
+        <div
+          style={{
+            fontSize: 60,
+            color: 'white',
+            background: `url(${encodeURIComponent(imageUrl)}?cb=${cacheBuster}) no-repeat center center`,
+            backgroundSize: 'cover',
+            width: '100%',
+            height: '100%',
+            padding: '40px',
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'center',
+          }}
+        >
+          {title}
+        </div>
+      ),
+      {
+        width: 1200,
+        height: 630,
+      }
+    );
+  } catch (error) {
+    console.error("Error generating image:", error);
+    return new Response("Error generating image", { status: 500 });
+  }
 }
--- app/all-articles/[id]/ArticlePage.jsx
+++ app/all-articles/[id]/ArticlePage.jsx
@@ -8,18 +8,16 @@ import moment from 'moment';
 import 'moment/locale/ka';
 import { FacebookShareButton, FacebookIcon, TwitterShareButton, TwitterIcon } from 'next-share';
 
-
 const categories = [
   { name: "სტატიები", path: "/all-articles" },
   { name: "თარგმანი", path: "/translate" },
   { name: "მაუწყებელი წიგნები", path: "/books" },
   { name: "თავისუფალი სვეტი", path: "/free-column" },
-
 ];
 
-
 async function fetchArticle(id) {
-  const res = await fetch(`https://mautskebeli.wpenginepowered.com/wp-json/wp/v2/article/${id}?acf_format=standard&_fields=id,title,acf,date&_=${new Date().getTime()}`);
+  const apiUrl = `https://mautskebeli.wpenginepowered.com/wp-json/wp/v2/article/${id}?acf_format=standard&_fields=id,title,acf,date&_=${new Date().getTime()}`;
+  const res = await fetch(apiUrl);
   if (!res.ok) {
     throw new Error('Failed to fetch article');
   }
@@ -45,13 +43,15 @@ const ArticlePage = ({ params }) => {
     setIsMounted(true);
     const getArticle = async () => {
       try {
+        console.log("Fetching article with ID:", id);
         const fetchedArticle = await fetchArticle(id);
+        console.log("Article fetched successfully:", fetchedArticle);
         setArticle({
           ...fetchedArticle,
           formattedDate: formatDate(fetchedArticle.date),
         });
       } catch (error) {
-        console.error(error);
+        console.error("Error fetching article:", error);
       }
     };
     if (id) {
@@ -100,6 +100,7 @@ const ArticlePage = ({ params }) => {
     if (article && articleContentRef.current) {
       const articleContent = articleContentRef.current;
       articleContent.innerHTML = DOMPurify.sanitize(article.acf['main-text']);
+      console.log("Article content sanitized and set.");
 
       // Apply styles for blockquotes
       const blockquotes = articleContent.querySelectorAll('blockquote');
@@ -125,9 +126,7 @@ const ArticlePage = ({ params }) => {
   }
 
   const articleUrl = `https://www.mautskebeli.ge/all-articles/${article.id}`;
-  const ogImage = article.acf.image
-  ? `${article.acf.image}?cb=${new Date().getTime()}`
-  : '/images/default-og-image.jpg';
+  const ogImage = article.acf.image ? `${article.acf.image}?cb=${new Date().getTime()}` : '/images/default-og-image.jpg';
 
   const sanitizeDescription = (html) => {
     const doc = new DOMParser().parseFromString(html, 'text/html');
@@ -136,79 +135,76 @@ const ArticlePage = ({ params }) => {
 
   const ogDescription = sanitizeDescription(article.acf['main-text']).slice(0, 150);
 
-  const currentCategory = "სტატიები"; 
+  const currentCategory = "სტატიები";
 
   return (
     <>
-   <Head>
-    <title>{article.title.rendered}</title>
-    <meta name="description" content={ogDescription} />
-    <meta property="og:url" content={articleUrl} />
-    <meta property="og:type" content="article" />
-    <meta property="og:title" content={article.title.rendered} />
-    <meta property="og:description" content={ogDescription} />
-    <meta property="og:image" content={ogImage} />
-    <meta property="og:image:width" content="1200" />
-    <meta property="og:image:height" content="630" />
-    <meta property="fb:app_id" content="2191957607826649" />
-    <meta property="og:image:alt" content={article.title.rendered} />
-  </Head>
+      <Head>
+        <title>{article.title.rendered}</title>
+        <meta name="description" content={ogDescription} />
+        <meta property="og:url" content={articleUrl} />
+        <meta property="og:type" content="article" />
+        <meta property="og:title" content={article.title.rendered} />
+        <meta property="og:description" content={ogDescription} />
+        <meta property="og:image" content={ogImage} />
+        <meta property="og:image:width" content="1200" />
+        <meta property="og:image:height" content="630" />
+        <meta property="fb:app_id" content="2191957607826649" />
+        <meta property="og:image:alt" content={article.title.rendered} />
+      </Head>
 
-    <section className="w-full mx-auto mt-10 px-4 lg:px-0 overflow-x-hidden relative">
-      <div className="w-full lg:w-[60%] mx-auto bg-opacity-90 p-5 rounded-lg">
-      <div className="lg:flex hidden justify-start rounded-md space-x-6 px-20 gap-10 py-4 mt-[-12px] mb-10 font-noto-sans-georgian w-full mx-auto bg-[#AD88C6]">
-               
-               {categories.map((category) => (
-                 <a
-                   key={category.name}
-                   href={category.path}
-                   className={`text-sm  text-[#474F7A] ${
-                     category.name === currentCategory
-                       ? "text-white font-bold"
-                       : "text-[#474F7A]  hover:scale-110"
-                   }`}
-                 >
-                   {category.name}
-                 </a>
-               ))}
-             </div>
-        <div className="w-full h-auto mb-5">
-          <Image src={article.acf.image || '/images/default-og-image.jpg'} alt={article.title.rendered} width={800} height={450} style={{ objectFit: 'cover' }} className="rounded-lg w-full" />
-          <h1 className="font-alk-tall-mtavruli text-[32px] sm:text-[64px] font-light leading-none text-[#474F7A] mt-[24px] mb-5">{article.title.rendered}</h1>
-          <h3 className="font-alk-tall-mtavruli text-[32px] sm:text-[34px] font-light leading-wide text-[#474F7A] mt-[24px] mb-5">{article.acf.sub_title}</h3>
-          <h2 className="font-noto-sans-georgian text-[16px] sm:text-[24px] font-extrabold text-[#AD88C6] leading-normal mb-5">{article.acf['ავტორი']}</h2>
-          <p className="text-[#474F7A] font-semibold pb-10">{article.formattedDate}</p>
-        </div>
-        <div ref={articleContentRef} className="article-content text-[#474F7A] text-wrap w-full font-noto-sans-georgian text-[14px] sm:text-[16px] font-normal lg:text-justify leading-[30px] sm:leading-[35px] tracking-[0.32px]"></div>
-        <div className="flex flex-wrap gap-4 mt-10">
-          <button onClick={() => setShowShareOptions(true)} className="bg-[#FECE27] text-[#474F7A] pl-[18px] pr-[18px] pt-[4px] pb-[4px] text-[16px] font-seibold rounded flex gap-[12px] items-center justify-center">
-            <Image src="/images/share.png" alt="share icon" width={24} height={24} />
-          </button>
-        </div>
-        {showShareOptions && (
-          <div className="fixed inset-0 bg-gray-800 bg-opacity-50 flex items-center justify-center">
-            <div ref={shareOptionsRef} className="rounded-lg p-6 w-80" style={{ backgroundColor: "rgba(0, 0, 0, 0.30)" }}>
-              <h2 className="text-xl text-white font-bold mb-4">გააზიარე</h2>
-              <div className="flex items-center pt-7 gap-5">
-                <FacebookShareButton url={articleUrl} quote={article.title.rendered}>
-                  <FacebookIcon size={44} round={true} />
-                </FacebookShareButton>
-                <TwitterShareButton url={articleUrl} title={article.title.rendered}>
-                  <TwitterIcon size={44} round={true} />
-                </TwitterShareButton>
+      <section className="w-full mx-auto mt-10 px-4 lg:px-0 overflow-x-hidden relative">
+        <div className="w-full lg:w-[60%] mx-auto bg-opacity-90 p-5 rounded-lg">
+          <div className="lg:flex hidden justify-start rounded-md space-x-6 px-20 gap-10 py-4 mt-[-12px] mb-10 font-noto-sans-georgian w-full mx-auto bg-[#AD88C6]">
+            {categories.map((category) => (
+              <a
+                key={category.name}
+                href={category.path}
+                className={`text-sm  text-[#474F7A] ${
+                  category.name === currentCategory ? "text-white font-bold" : "text-[#474F7A] hover:scale-110"
+                }`}
+              >
+                {category.name}
+              </a>
+            ))}
+          </div>
+          <div className="w-full h-auto mb-5">
+            <Image src={article.acf.image || '/images/default-og-image.jpg'} alt={article.title.rendered} width={800} height={450} style={{ objectFit: 'cover' }} className="rounded-lg w-full" />
+            <h1 className="font-alk-tall-mtavruli text-[32px] sm:text-[64px] font-light leading-none text-[#474F7A] mt-[24px] mb-5">{article.title.rendered}</h1>
+            <h3 className="font-alk-tall-mtavruli text-[32px] sm:text-[34px] font-light leading-wide text-[#474F7A] mt-[24px] mb-5">{article.acf.sub_title}</h3>
+            <h2 className="font-noto-sans-georgian text-[16px] sm:text-[24px] font-extrabold text-[#AD88C6] leading-normal mb-5">{article.acf['ავტორი']}</h2>
+            <p className="text-[#474F7A] font-semibold pb-10">{article.formattedDate}</p>
+          </div>
+          <div ref={articleContentRef} className="article-content text-[#474F7A] text-wrap w-full font-noto-sans-georgian text-[14px] sm:text-[16px] font-normal lg:text-justify leading-[30px] sm:leading-[35px] tracking-[0.32px]"></div>
+          <div className="flex flex-wrap gap-4 mt-10">
+            <button onClick={() => setShowShareOptions(true)} className="bg-[#FECE27] text-[#474F7A] pl-[18px] pr-[18px] pt-[4px] pb-[4px] text-[16px] font-seibold rounded flex gap-[12px] items-center justify-center">
+              <Image src="/images/share.png" alt="share icon" width={24} height={24} />
+            </button>
+          </div>
+          {showShareOptions && (
+            <div className="fixed inset-0 bg-gray-800 bg-opacity-50 flex items-center justify-center">
+              <div ref={shareOptionsRef} className="rounded-lg p-6 w-80" style={{ backgroundColor: "rgba(0, 0, 0, 0.30)" }}>
+                <h2 className="text-xl text-white font-bold mb-4">გააზიარე</h2>
+                <div className="flex items-center pt-7 gap-5">
+                  <FacebookShareButton url={articleUrl} quote={article.title.rendered}>
+                    <FacebookIcon size={44} round={true} />
+                  </FacebookShareButton>
+                  <TwitterShareButton url={articleUrl} title={article.title.rendered}>
+                    <TwitterIcon size={44} round={true} />
+                  </TwitterShareButton>
+                </div>
               </div>
             </div>
-          </div>
+          )}
+        </div>
+        {showScrollButton && (
+          <button onClick={scrollToTop} className="fixed bottom-10 right-10 text-[#474F7A] w-[210px] h-[51px] rounded-[100px] inline-flex items-center justify-center gap-[10px]"
+            style={{ padding: '25px 32px 28px 32px', background: '#FECE27', boxShadow: '0 0 20px 5px rgba(254, 206, 39, 0.5)' }}>
+            <span className="whitespace-nowrap">საწყისზე დაბრუნება</span>
+          </button>
         )}
-      </div>
-      {showScrollButton && (
-        <button onClick={scrollToTop} className="fixed bottom-10 right-10 text-[#474F7A] w-[210px] h-[51px] rounded-[100px] inline-flex items-center justify-center gap-[10px]"
-          style={{ padding: '25px 32px 28px 32px', background: '#FECE27', boxShadow: '0 0 20px 5px rgba(254, 206, 39, 0.5)' }}>
-          <span className="whitespace-nowrap">საწყისზე დაბრუნება</span>
-        </button>
-      )}
-      <footer ref={footerRef} className="h-[100px]"></footer>
-    </section>
+        <footer ref={footerRef} className="h-[100px]"></footer>
+      </section>
     </>
   );
 };
