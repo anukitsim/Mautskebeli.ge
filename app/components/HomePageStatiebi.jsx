f8b6ab6c54803c23eb7353159f44332e23482e5a
anukitsim <104069419+anukitsim@users.noreply.github.com>
2024-09-20 14:10:44 +0400

    fixed article card sizing on home page

--- app/components/HomePageStatiebi.jsx
+++ app/components/HomePageStatiebi.jsx
@@ -80,58 +80,40 @@ const HomePageStatiebi = () => {
 
   return (
     <section className="mx-auto mt-[110px] flex flex-col">
-      <style jsx global>{`
-        @media (max-width: 768px) {
-          .article:first-child {
-            flex: 0 0 100%;
-          }
-
-          .articles-container {
-            flex-wrap: nowrap;
-          }
-
-          .article:not(:first-child){
-            flex: 0 0 100%;
-          }
-
-          .article {
-            flex: 0 0 auto;
-          }
-        }
-      `}</style>
-      <div className="w-full sm:w-10/12 flex items-center justify-between mx-auto pl-4 pr-4 lg:pl-2 lg:pr-2">
+      <div className="w-full sm:w-10/12 flex items-center justify-between mx-auto px-4 lg:px-2">
         <p className="text-[#474F7A] text-[24px] font-bold">სტატიები</p>
         <Link href='/all-articles' className="text-[#474F7A] text-[14px] font-semibold">ნახე ყველა</Link>
       </div>
-      <div className="w-10/12 mx-auto flex articles-container overflow-x-auto mt-5 flex-row gap-5">
+      <div className="w-10/12 mx-auto grid grid-cols-1 sm:grid-cols-3 gap-5 mt-5">
         {articles.map(article => {
           const imageUrl = article.acf.image ? article.acf.image : '/images/default-image.png';
           return (
             <Link href={`/all-articles/${article.id}`} passHref key={article.id}>
-              <div className="article bg-[#F6F4F8] rounded-tl-[10px] rounded-tr-[10px] border border-[#B6A8CD] overflow-hidden" style={{ minWidth: '300px' }}>
-                <div className="article-image-container relative w-full h-[200px]">
+              <div className="bg-[#F6F4F8] rounded-tl-[10px] rounded-tr-[10px] border border-[#B6A8CD] overflow-hidden flex flex-col">
+                <div className="relative w-full h-[200px]">
                   <Image
                     src={imageUrl}
                     alt="article-cover"
                     fill
                     sizes="(max-width: 768px) 100vw, 33vw"
-                    style={{ objectFit: 'cover'}} // Correct usage in Next.js 13
-                    className="article-image"
+                    className="object-cover"
                     priority
                   />
                 </div>
-                <div className="p-[18px]">
-                  <h2 className="text-[20px] font-bold mb-2" style={{ color: '#474F7A' }}>
-                    {article.title.rendered}
-                  </h2>
-                  <span className="text-[#8D91AB] text-[14px] font-bold">
-                    {article.acf['ავტორი']}
-                  </span>
-                  <p className="text-sm pt-[18px]" style={{ color: '#000' }}>
-                    {truncateText(stripHtml(article.acf['main-text']), 30)}
-                  </p>
-                  <div className="flex flex-col justify-end pt-[30px] items-end">
-                    <button className="text-white text-[12px] mt-[16px] bg-[#AD88C6] rounded-[6px] pt-[10px] pb-[10px] pl-[12px] pr-[12px]">
+                <div className="p-4 flex flex-col justify-between flex-grow">
+                  <div>
+                    <h2 className="text-[20px] font-bold mb-2 text-[#474F7A]">
+                      {article.title.rendered}
+                    </h2>
+                    <span className="text-[#8D91AB] text-[14px] font-bold">
+                      {article.acf['ავტორი']}
+                    </span>
+                    <p className="text-sm pt-[18px] text-black">
+                      {truncateText(stripHtml(article.acf['main-text']), 30)}
+                    </p>
+                  </div>
+                  <div className="flex justify-end pt-4">
+                    <button className="text-white text-[12px] bg-[#AD88C6] rounded-[6px] py-2 px-4">
                       ნახეთ სრულად
                     </button>
                   </div>
