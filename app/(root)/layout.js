d2d99930c55784c968bc157fbbaeb582ffdecc84
anukitsim <104069419+anukitsim@users.noreply.github.com>
2024-09-24 20:41:59 +0400

    google analytics script correctly appearing in head tag

--- app/(root)/layout.js
+++ app/(root)/layout.js
@@ -3,17 +3,17 @@ import Header from "../components/Header";
 import Navigation from "../components/Navigation";
 import { MenuProvider } from "@/app/context/MenuContext";
 import Footer from "../components/Footer";
-import Head from 'next/head';
-import Script from 'next/script'; // Import Next.js Script component
+import Head from "next/head";
+import Script from "next/script";
 
 export const metadata = {
   title: "მაუწყებელი",
   description: "მედია პლათფორმა მაუწყებელი",
-  app_id: "1819807585106457"
+  app_id: "1819807585106457",
 };
 
 export default function RootLayout({ children }) {
-  const defaultOgImage = 'https://mautskebeli.ge/api/og?title=Default%20Title';
+  const defaultOgImage = "https://mautskebeli.ge/api/og?title=Default%20Title";
 
   return (
     <html lang="en">
@@ -26,30 +26,28 @@ export default function RootLayout({ children }) {
         <meta property="fb:app_id" content="1819807585106457" />
         <meta property="og:image:width" content="1200" />
         <meta property="og:image:height" content="630" />
+      </Head>
+      <body>
+        {/* Your actual content goes here */}
+        {children}
 
-        {/* Google Analytics Script */}
+        {/* Google Analytics script moved inside the <body> */}
         <Script
-          src={`https://www.googletagmanager.com/gtag/js?id=G-MZY3EGWH6J`}  // Replace with your Measurement ID
-          strategy="beforeInteractive" // Load before the page becomes interactive
+          src="https://www.googletagmanager.com/gtag/js?id=G-MZY3EGWH6J"
+          strategy="afterInteractive"
         />
         <Script
           id="ga-init"
-          strategy="beforeInteractive" // Load before the page becomes interactive
+          strategy="afterInteractive"
           dangerouslySetInnerHTML={{
             __html: `
               window.dataLayer = window.dataLayer || [];
               function gtag(){dataLayer.push(arguments);}
               gtag('js', new Date());
-              gtag('config', 'G-MZY3EGWH6J');  // Replace with your Measurement ID
+              gtag('config', 'G-MZY3EGWH6J');
             `,
           }}
         />
-      </Head>
-      <head>
-        <link rel="icon" href="/favicon.ico" />
-      </head>
-      <body>
-        {children}
       </body>
     </html>
   );
