c1ab853bb8a803796d95cecb9b169ee9b981dca7
anukitsim <104069419+anukitsim@users.noreply.github.com>
2024-12-19 13:40:17 +0400

    Disable Vercel Image Optimization to prevent overage charges

--- next.config.js
+++ next.config.js
@@ -5,16 +5,18 @@ const redirects = require('./redirects.json');
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
+    unoptimized: true, // Disables Vercel's Image Optimization
     domains: [
       'mautskebeli.wpenginepowered.com',
       'img.youtube.com',
       'www.mautskebeli.ge',
-      'media.mautskebeli.ge'
+      'media.mautskebeli.ge' // Cloudflare R2 domain
     ],
-    formats: ['image/avif', 'image/webp'],
+    formats: ['image/avif', 'image/webp'], // Optional: Keep if pre-optimizing via Cloudflare
     deviceSizes: [320, 420, 768, 1024, 1200],
     imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
-    loader: 'default',
+    // Remove or keep the loader as 'default' since 'unoptimized' is set to true
+    // loader: 'default',
   },
   async redirects() {
     return redirects.map(({ source, destination }) => {
