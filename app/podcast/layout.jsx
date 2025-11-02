59e9c2ece0a479c328860565e66b172670300660
anukitsim <104069419+anukitsim@users.noreply.github.com>
2025-11-02 16:03:17 +0100

    Add missing layouts with Google Analytics for all pages

---
+++ app/podcast/layout.jsx
@@ -0,0 +1,49 @@
+import "../../style/globals.css";
+import Header from "../components/Header";
+import Navigation from "../components/Navigation";
+import { MenuProvider } from "@/app/context/MenuContext";
+import Footer from "../components/Footer";
+import Script from 'next/script';
+import AnalyticsPageView from '../components/AnalyticsPageView';
+
+export const metadata = {
+  title: "პოდკასტი - მაუწყებელი",
+  description: "მაუწყებლის პოდკასტები",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body>
+        <MenuProvider>
+          <div className="sticky top-0 z-50">
+            <Header />
+            <Navigation />
+          </div>
+          {children}
+          <Footer />
+        </MenuProvider>
+        
+        {/* Google Analytics */}
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-C2ZPMYP4FY"
+          strategy="afterInteractive"
+        />
+        <Script
+          id="ga-init-podcast"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-C2ZPMYP4FY', { send_page_view: false });
+            `
+          }}
+        />
+        <AnalyticsPageView />
+      </body>
+    </html>
+  );
+}
+
---
+++ app/xelovneba/layout.jsx
@@ -0,0 +1,49 @@
+import "../../style/globals.css";
+import Header from "../components/Header";
+import Navigation from "../components/Navigation";
+import { MenuProvider } from "@/app/context/MenuContext";
+import Footer from "../components/Footer";
+import Script from 'next/script';
+import AnalyticsPageView from '../components/AnalyticsPageView';
+
+export const metadata = {
+  title: "ხელოვნება - მაუწყებელი",
+  description: "ხელოვნების სიახლეები და ამბები",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body>
+        <MenuProvider>
+          <div className="sticky top-0 z-50">
+            <Header />
+            <Navigation />
+          </div>
+          {children}
+          <Footer />
+        </MenuProvider>
+        
+        {/* Google Analytics */}
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-C2ZPMYP4FY"
+          strategy="afterInteractive"
+        />
+        <Script
+          id="ga-init-xelovneba"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-C2ZPMYP4FY', { send_page_view: false });
+            `
+          }}
+        />
+        <AnalyticsPageView />
+      </body>
+    </html>
+  );
+}
+
